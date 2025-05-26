bdd4fd55153b94d393371b3a234bd424b783a1e8
AdnaneTaleb10 <adnane102464@gmail.com>
2025-05-26 10:55:12 +0100

    Add logo in header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './css/styles.css';
+import "./css/style.css";
+import logo from "./Assets/to-do-list.png";
+
+let logoContainer = document.querySelector("#logo");
+const favIcon = document.createElement("img");
+
+favIcon.src = logo;
+logoContainer.appendChild(favIcon);
